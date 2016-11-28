ba017a62f0addb33aaccc81fcf0e4473a705d77c
imota <mota.icaro@gmail.com>
2016-11-28 19:03:47 -0200

    Speed publisher subscribe to the keyboard node.

--- scripts/speed_publisher.py
+++ scripts/speed_publisher.py
@@ -3,11 +3,18 @@
 import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+from p3at_tutorial.msg import KeyboardMessage
+
+def callback(data):
+    rospy.loginfo('I heard %s', data.key)
 
 def speed_publisher():
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rospy.init_node('speed_publisher', anonymous=True)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
+    
+    rospy.Subscriber('keyboard_topic', KeyboardMessage, callback, queue_size = 1)
+
     
     while not rospy.is_shutdown():
         vel_lin = 1
@@ -16,7 +23,7 @@ def speed_publisher():
         speed = Twist()
         speed.linear = Vector3(vel_lin,0,0)
         speed.angular = Vector3(0,0,vel_ang)
-        rospy.loginfo(speed)
+        #rospy.loginfo(speed)
         pub.publish(speed)
         rate.sleep()
 
