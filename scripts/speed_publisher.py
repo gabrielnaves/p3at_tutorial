2bbe1db4660770a35b5f80f4369b8f8c1ab7a2ee
imota <mota.icaro@gmail.com>
2016-11-28 19:50:51 -0200

    We can now control the robot through the keyboard

--- scripts/speed_publisher.py
+++ scripts/speed_publisher.py
@@ -4,26 +4,47 @@ import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 from p3at_tutorial.msg import KeyboardMessage
+from enum import Enum
+import math
+
+up = 119
+down = 115
+right = 100
+left = 97
+
+vel_lin = 0
+vel_ang = 0
 
 def callback(data):
-    rospy.loginfo('I heard %s', data.key)
+    global vel_lin
+    global vel_ang
+    if data.key == up:
+        vel_lin = 1;
+        vel_ang = 0;
+    if data.key == down:
+        vel_lin = -1;
+        vel_ang = 0;
+    if data.key == right:
+        vel_lin = 0;
+        vel_ang = -math.pi/2;
+    if data.key == left:
+        vel_lin = 0;
+        vel_ang = math.pi/2;
 
 def speed_publisher():
+    global vel_lin
+    global vel_ang
+
     rospy.init_node('speed_publisher', anonymous=True)
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
     
     rospy.Subscriber('keyboard_topic', KeyboardMessage, callback, queue_size = 1)
-
     
     while not rospy.is_shutdown():
-        vel_lin = 1
-        vel_ang = 0
-
         speed = Twist()
         speed.linear = Vector3(vel_lin,0,0)
         speed.angular = Vector3(0,0,vel_ang)
-        #rospy.loginfo(speed)
         pub.publish(speed)
         rate.sleep()
 
